e7beee1be03308a20b36acec70bc39244a9622c8
Proloy Karmakar <proloy@gmail.com>
2023-03-22 01:16:49 +0600

    I have created a new java class named constructor

---
+++ app/src/main/java/com/example/jobportalapp/Constructor.java
@@ -0,0 +1,17 @@
+package com.example.jobportalapp;
+
+public class Constructor {
+    private String name;
+
+    public Constructor(String string) {
+        name = string;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
