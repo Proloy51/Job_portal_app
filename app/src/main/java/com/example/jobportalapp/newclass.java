d53ca7f6512848658c278392a2866076be011363
Proloy Karmakar <proloy@gmail.com>
2023-03-22 01:44:28 +0600

    Changed

--- app/src/main/java/com/example/jobportalapp/newclass.java
+++ app/src/main/java/com/example/jobportalapp/newclass.java
@@ -5,5 +5,6 @@ public class newclass {
         System.out.println("This is our first lab");
         System.out.println("I am doing my assignment for LAb1");
         System.out.println("");
+        System.out.println("This is for conflict");
     }
 }
