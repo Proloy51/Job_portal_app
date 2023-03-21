5f61210a842ca7a58ed852ca6f83193a343e28db
Proloy Karmakar <proloy@gmail.com>
2023-03-22 01:36:37 +0600

    modified

--- app/src/main/java/com/example/jobportalapp/newclass.java
+++ app/src/main/java/com/example/jobportalapp/newclass.java
@@ -3,5 +3,6 @@ package com.example.jobportalapp;
 public class newclass {
     public static void main(String[] args) {
         System.out.println("This is our first lab");
+        System.out.println("I am doing my assignment for LAb1");
     }
 }
